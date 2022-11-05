1a4d806a5118185bbd826859480f8c05ce4d10c3
Marius Cornescu <marius_cornescu@yahoo.com>
2022-11-05 13:52:55 +0100

    Refactor to protocol object

--- Artizan-CommProtocol.cpp
+++ Artizan-CommProtocol.cpp
@@ -8,11 +8,90 @@
 #include "Artizan-CommProtocol.h"
 
 
-
-
+//oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 RtznCommProtocol::RtznCommProtocol() {
-  this->haveToPublish = false; // have to inform 3rd party (not partner)
+    this->nodeRole = "";
+    this->haveToPublish = false; // have to inform 3rd party (not partner)
 }
+RtznCommProtocol::RtznCommProtocol(const char *nodeRole) {
+    this->nodeRole = nodeRole;
+    this->haveToPublish = false; // have to inform 3rd party (not partner)
+}
+//oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+
+//==================================================================================================
+bool RtznCommProtocol::hasMessageInInboxThenReadMessage() {
+    int replySize = this->receiveData();
+    this->__logDebug("PARTNER reply size =", replySize);
+
+    if (replySize > 0) {
+        this->__logMessage("Receive data", messageIn);
+        return true;
+    } else {
+        this->__logDebug("Receive NO data from PARTNER");
+        return false;
+    }
+}
+//==================================================================================================
+void RtznCommProtocol::commActOnMessage() {
+    this->__logDebug("commActOnMessage");
+
+    switch (this->messageIn[MESSAGE_COMMAND_CODE_INDEX]) {
+        case COMMAND_NOP: /*DO NOTHING*/ break;
+        case COMMAND_PUSH: this->commActOnPushMessage(); break;
+        case COMMAND_POLL: this->commActOnPollMessage(); break;
+        case COMMAND_PUSH_POLL: this->commActOnPushPollMessage(); break;
+        default: /*UNKNOWN*/ break;
+    }
+}
+//==================================================================================================
+void RtznCommProtocol::commActOnPushMessage() {
+    /* The PARTNER is informing me of a status change */
+    this->__logDebug("commActOnPushMessage:");
+
+    byte newVentSpeed = this->messageIn[MESSAGE_PAYLOAD_START_INDEX];
+    if (newVentSpeed != getValue1()) {
+        setValue1(newVentSpeed);
+        this->haveToPublish = true;
+    }
+
+    byte newActionCode = this->messageIn[MESSAGE_PAYLOAD_START_INDEX + 1];
+    if (newActionCode != getValue2()) {
+        setValue2(newActionCode);
+        this->haveToPublish = true;
+    }
+}
+//==================================================================================================
+void RtznCommProtocol::commActOnPollMessage() {
+    /* The PARTNER is asking for my status */
+    /* The body of the request is empty */
+    this->__logDebug("commActOnPollMessage: Send data to PARTNER");
+
+    // Send data to PARTNER
+    char message[MESSAGE_SIZE];
+    char payload[] = { (char)getValue1(), (char)getValue2() };
+    this->newMessage(message, COMMAND_PUSH, payload, 2);
+    this->sendMessage(message);
+
+    this->__logMessage("Sent", message);
+}
+//==================================================================================================
+void RtznCommProtocol::commActOnPushPollMessage() {
+    /* The PARTNER is informing me of a status change */
+    this->commActOnPushMessage();
+    /* The PARTNER is asking for my status */
+    this->commActOnPollMessage();
+}
+//==================================================================================================
+
+//==================================================================================================
+bool RtznCommProtocol::isHaveToPublish() {
+    return this->haveToPublish;
+}
+void RtznCommProtocol::setHaveToPublish(bool haveToPublish) {
+    this->haveToPublish = haveToPublish;
+}
+//==================================================================================================
 
 
 //==================================================================================================
@@ -64,33 +143,26 @@ bool RtznCommProtocol::sendMessage(char *data) {
 
 
 
-
-
 //==================================================================================================
 /**
   * 
   */
 void RtznCommProtocol::__purgeDataLine(int maxToPurge, bool indiscriminate) {
+    this->__logDebug("Purge up to", maxToPurge);
+
+    byte purgeCount = 0;
+    char char1 = 0;
+    while (Serial.available() > 0 && purgeCount <= maxToPurge && (indiscriminate || (char1 != MESSAGE_END && char1 != MESSAGE_PREFIX1))) {
+        char1 = Serial.read();
 #ifdef DEBUG
-  Serial.print("Purge up to [");
-  Serial.print(maxToPurge);
-  Serial.println("]");
+        Serial.println(char1, DEC);
 #endif
-  byte purgeCount = 0;
-  char char1 = 0;
-  while (Serial.available() > 0 && purgeCount <= maxToPurge && (indiscriminate || (char1 != MESSAGE_END && char1 != MESSAGE_PREFIX1))) {
-    char1 = Serial.read();
+        purgeCount = purgeCount + 1;
+    }
 #ifdef DEBUG
-    Serial.println(char1, DEC);
-#endif
-    purgeCount = purgeCount + 1;
-  }
-#ifdef DEBUG
-  if (purgeCount > 0) {
-    Serial.print("PURGED: [");
-    Serial.print(purgeCount);
-    Serial.println("]");
-  }
+    if (purgeCount > 0) {
+        this->__logDebug("Purged:", purgeCount);
+    }
 #endif
 }
 //==================================================================================================
@@ -116,6 +188,40 @@ bool RtznCommProtocol::__isValidMessage() {
   return (char1 == MESSAGE_PREFIX1 && char2 == MESSAGE_PREFIX2);
 }
 //==================================================================================================
-
-
-
+void RtznCommProtocol::__logDebug(const char *label) {
+#ifdef DEBUG
+  Serial.print(this->nodeRole);
+  Serial.print(": ");
+  Serial.println(label);
+#endif
+}
+//==================================================================================================
+void __logDebug(const char *label1, int label2) {
+#ifdef DEBUG
+  Serial.print(this->nodeRole);
+  Serial.print(": ");
+  Serial.print(label1);
+  Serial.print(" [");
+  Serial.print(label2);
+  Serial.println("]");
+#endif
+}
+//==================================================================================================
+void RtznCommProtocol::__logMessage(const char *label, char *message) {
+#ifdef DEBUG
+  Serial.print(this->nodeRole);
+  Serial.print(": ");
+  Serial.print(label);
+  Serial.print(" [0x");
+  Serial.print(message[MESSAGE_COMMAND_CODE_INDEX], HEX);
+  Serial.print("](");
+  Serial.print(message[MESSAGE_PAYLOAD_START_INDEX], DEC);
+  Serial.print(")\t[");
+  for (int i = MESSAGE_PAYLOAD_START_INDEX; i < MESSAGE_SIZE; i++) {
+    Serial.print((char)message[i]);
+    Serial.print("|");
+  }
+  Serial.println("]");
+#endif
+}
+//==================================================================================================
--- Artizan-CommProtocol.h
+++ Artizan-CommProtocol.h
@@ -1,12 +1,20 @@
 #ifndef _HEADERFILE_COMM_PROTOCOL
 #define _HEADERFILE_COMM_PROTOCOL
 
-// Define message size in bytes
-#define MESSAGE_SIZE 12
-#define MESSAGE_RAW_SIZE 15  // MESSAGE_SIZE + 2 prefix + 1 end
+#if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+#else
+    #include "WProgram.h"
+#endif
 
-#define MESSAGE_COMMAND_CODE_INDEX 0
-#define MESSAGE_PAYLOAD_START_INDEX 1
+#include <stdint.h>
+
+// Define message size in bytes
+const byte MESSAGE_SIZE = 12;
+const byte MESSAGE_RAW_SIZE = 15;  // MESSAGE_SIZE + 2 prefix + 1 end
+
+const byte MESSAGE_COMMAND_CODE_INDEX = 0;
+const byte MESSAGE_PAYLOAD_START_INDEX = 1;
 
 //= CONSTANTS ======================================================================================
 //------------------------------------------------
@@ -23,8 +31,6 @@ const char COMMAND_PUSH = 30;       // INFORM PARTNER
 const char COMMAND_PUSH_POLL = 40;  // INFORM PARTNER AND REQUEST UPDATE
 //
 //= VARIABLES ======================================================================================
-char messageIn[MESSAGE_SIZE];
-//------------------------------------------------
 byte getValue1();
 void setValue1(byte value1);
 byte getValue2();
@@ -36,129 +42,44 @@ class RtznCommProtocol {
 
   public:
     RtznCommProtocol();
+    RtznCommProtocol(const char *nodeRole);
 
     char messageIn[MESSAGE_SIZE];
 
+    bool hasMessageInInboxThenReadMessage();
+
+    void commActOnMessage();
+    void commActOnPushMessage();
+    void commActOnPollMessage();
+    void commActOnPushPollMessage();
+
+    bool isHaveToPublish();
+    void setHaveToPublish(bool haveToPublish);
+
+    //#if not defined( ArtizanCommProtocolDisableReceiving )
+    //void setReceiveTolerance(int nPercent);
+    //#endif
+
+  private:
     int receiveData();
     void newMessage(char *data, char commandCode, char *payload, byte payloadSize);
     bool sendMessage(char *data);
 
-
-
-    
-    void switchOff(const char* sGroup, int nSwitchNumber);
-    #if not defined( ArtizanCommProtocolDisableReceiving )
-    void setReceiveTolerance(int nPercent);
-    #endif
-
-  private:
     void __purgeDataLine(int maxToPurge, bool indiscriminate);
     bool __isValidMessage();
 
+    // debugging
+    void __logDebug(const char *label);
+    void __logDebug(const char *label1, int label2);
+
+    void __logMessage(const char *label, char *message);
+
     bool haveToPublish;
+    const char *nodeRole;
 
-    #if not defined( ArtizanCommProtocolDisableReceiving )
-    static void handleInterrupt();
-    #endif
+    //#if not defined( ArtizanCommProtocolDisableReceiving )
+    //static void handleInterrupt();
+    //#endif
 };
-
-
-//==================================================================================================
-bool hasMessageInInboxThenReadMessage() {
-  int replySize = receiveData();
-#ifdef DEBUG
-  Serial.print(COMM_ROLE);
-  Serial.print(": PARTNER reply size = [");
-  Serial.print(replySize);
-  Serial.println("]");
-#endif
-
-  if (replySize > 0) {
-    printMessage(COMM_ROLE, ": Receive data", messageIn, MESSAGE_SIZE);
-    return true;
-  } else {
-#ifdef DEBUG
-    Serial.print(COMM_ROLE);
-    Serial.println(": Receive NO data from PARTNER");
-#endif
-    return false;
-  }
-}
-//==================================================================================================
-void commActOnMessage() {
-#ifdef DEBUG
-  Serial.println("commActOnMessage");
-#endif
-  switch (messageIn[MESSAGE_COMMAND_CODE_INDEX]) {
-    case COMMAND_NOP: /*DO NOTHING*/ break;
-    case COMMAND_PUSH: commActOnPushMessage(); break;
-    case COMMAND_POLL: commActOnPollMessage(); break;
-    case COMMAND_PUSH_POLL: commActOnPushPollMessage(); break;
-    default: /*UNKNOWN*/ break;
-  }
-}
-//==================================================================================================
-void commActOnPushMessage() {
-  /* The PARTNER is informing me of a status change */
-#ifdef DEBUG
-  Serial.print(COMM_ROLE);
-  Serial.println(":commActOnPushMessage:");
-#endif
-  byte newVentSpeed = messageIn[MESSAGE_PAYLOAD_START_INDEX];
-  if (newVentSpeed != getValue1()) {
-    setValue1(newVentSpeed);
-    haveToPublish = true;
-  }
-
-  byte newActionCode = messageIn[MESSAGE_PAYLOAD_START_INDEX + 1];
-  if (newActionCode != getValue2()) {
-    setValue2(newActionCode);
-    haveToPublish = true;
-  }
-}
-//==================================================================================================
-void commActOnPollMessage() {
-  /* The PARTNER is asking for my status */
-  /* The body of the request is empty */
-#ifdef DEBUG
-  Serial.print(COMM_ROLE);
-  Serial.println(":commActOnPollMessage: Send data to PARTNER");
-#endif
-
-  // Send data to PARTNER
-  char message[MESSAGE_SIZE];
-  char payload[] = { (char)getValue1(), (char)getValue2() };
-  newMessage(message, COMMAND_PUSH, payload, 2);
-  sendMessage(message);
-
-  printMessage(COMM_ROLE, ": Sent", message, MESSAGE_SIZE);
-}
-//==================================================================================================
-void commActOnPushPollMessage() {
-  /* The PARTNER is informing me of a status change */
-  /* The PARTNER is asking for my status */
-  commActOnPushMessage();
-  commActOnPollMessage();
-}
-//==================================================================================================
-
-//==================================================================================================
-void printMessage(const char *prefix, const char *label, char *data, int size) {
-#ifdef DEBUG
-  Serial.print(prefix);
-  Serial.print(label);
-  Serial.print(" [0x");
-  Serial.print(data[MESSAGE_COMMAND_CODE_INDEX], HEX);
-  Serial.print("](");
-  Serial.print(data[1], DEC);
-  Serial.print(")\t[");
-  for (int i = 2; i < size; i++) {
-    Serial.print((char)data[i]);
-    Serial.print("|");
-  }
-  Serial.println("]");
-#endif
-}
-//==================================================================================================
 
 #endif  // _HEADERFILE_COMM_PROTOCOL
